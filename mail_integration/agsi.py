bfc9efc53d1f66bd7a740be2db464d769731d9d3
CurvaXS <suxarik24@gmail.com>
2024-09-11 18:43:08 +0300

    init

---
+++ mail_integration/agsi.py
@@ -0,0 +1,18 @@
+# mail_integration/asgi.py
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from mail_app import routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mail_integration.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
